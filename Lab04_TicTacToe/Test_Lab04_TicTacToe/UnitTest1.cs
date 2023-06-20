8d7c85df8816351c29c9ba00e6b17f1d93ebe82e
Nadine Aleideh <nadinealeideh75@gmail.com>
2023-06-20 18:07:18 +0300

    Add project files.

---
+++ Lab04_TicTacToe/Test_Lab04_TicTacToe/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Lab04_TicTacToe.Classes;
+
+namespace Test_Lab04_TicTacToe
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
