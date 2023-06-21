c82b8e2931e8b86190490dad17677e31ba6b0138
Nadine Aleideh <nadinealeideh75@gmail.com>
2023-06-21 15:45:26 +0300

    lab04 is ready

--- Lab04_TicTacToe/Lab04_TicTacToe/Classes/Player.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Classes/Player.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Lab04_TicTacToe.Classes
 {
-    class Player
+     public class Player
     {
 		public string Name { get; set; }
 		/// <summary>
@@ -18,21 +18,47 @@ namespace Lab04_TicTacToe.Classes
 		public bool IsTurn { get; set; }
 
 
-		public Position GetPosition(Board board)
+        /// <summary>
+        /// Initializes a new instance of the Player class with the specified name and marker.
+        /// </summary>
+        /// <param name="name">The player's name</param>
+        /// <param name="marker">The player's marker (X or O)</param>
+        public Player(string name, string marker)
+        {
+            Name = name;
+            Marker = marker;
+        }
+
+
+
+        /// <summary>
+        /// Retrieves the position chosen by the player on the game board. and used in TakeTurn method.
+        /// </summary>
+        /// <param name="board">The current game board</param>
+        /// <returns>The chosen position on the board</returns>
+        public Position GetPosition(Board board)
 		{
 			Position desiredCoordinate = null;
 			while (desiredCoordinate is null)
 			{
-				Console.WriteLine("Please select a location");
+				Console.Write("...Please select a location:");
 				Int32.TryParse(Console.ReadLine(), out int position);
-				desiredCoordinate = PositionForNumber(position);
+                Console.WriteLine();
+                desiredCoordinate = PositionForNumber(position);
 			}
 			return desiredCoordinate;
 
 		}
 
 
-		public static Position PositionForNumber(int position)
+
+        /// <summary>
+        /// Converts the selected number representation of a position to the corresponding row and column on the game board.and used in GetPosition method.
+        /// </summary>
+        /// <param name="position">The number representation of a position on the game board</param>
+        /// <returns>The corresponding position on the game board</returns>
+
+        public  Position PositionForNumber(int position)
 		{
 			switch (position)
 			{
@@ -50,23 +76,27 @@ namespace Lab04_TicTacToe.Classes
 			}
 		}
 
-	
-		public void TakeTurn(Board board)
+        /// <summary>
+        /// Allows the current player to take their turn by selecting a locatin on the game board. and it filps the player turns flag this helps switching btween players. and it is used in play method.
+        /// </summary>
+        /// <param name="board">The current game board</param>
+        public void TakeTurn(Board board)
 		{
 			IsTurn = true;
 
-			Console.WriteLine($"{Name} it is your turn");
+			Console.Write($"\n{Name} it is your turn");
 
 			Position position = GetPosition(board);
 
-			if (Int32.TryParse(board.GameBoard[position.Row, position.Column], out int _))
+			if (Int32.TryParse(board.GameBoard[position.Row, position.Column], out int _)) //if this selected position still a number(avalabile)
 			{
-				board.GameBoard[position.Row, position.Column] = Marker;
+				board.GameBoard[position.Row, position.Column] = Marker; //assign the current player marker to it 
 			}
 			else
 			{
-				Console.WriteLine("This space is already occupied");
-			}
-		}
+				Console.Write("This space is already occupied..try again");
+                 position = GetPosition(board); //i think this line of code should be here to allow the current player to pick an avalabile location otherwise without playing and picking a location and that's not fair
+            }
+        }
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Lab04_TicTacToe
+# TicTacToe Console App
+
+## What is TicTacToe?
+
+A 3x3 grid is used for the traditional paper and pencil game of tic tac toe. Two players alternately fill in grid squares with their respective symbols (often a 'X' and a 'O') until one player creates a winning pattern or all spaces are filled, in which case the game is a draw. This project uses C# to implement the TicTacToe game on a console.
+
+## Preview
+
+Here's a samle of what the TicTacToe game looks like when played:
+
+![gamelook](./assets/TicTacToep1.PNG)
+![gamelook](./assets/TicTacToep2.PNG)
+![gamelook](./assets/TicTacToepdraw.PNG)
+
+## How to Use
+
+To play the TicTacToe game, follow these steps:
+
+1. Clone the repository or download the source code.
+
+2. Open the project in your preferred development environment (e.g., Visual Studio).
+
+3. Build the solution to ensure all dependencies are resolved.
+
+4. Run the application.
+
+5. The game will prompt for player to insert their names and assign 'X' and 'O' symbols to each player.
+
+6. The game board will be displayed, and players will take turns entering their moves.
+
+7. Enter the number corresponding to the desired position on the grid (1-9).
+
+8. The game will update the board and continue until a winner is determined or the game ends in a draw.
+
+9. The final result (winner or draw) will be displayed, and players can choose to play again or exit the game.
+
+### Additional Details
+
+Automated unit tests are included to verify the correctness of critical game functions.
+
+![gametest](./assets/TicTacToetest.PNG)
